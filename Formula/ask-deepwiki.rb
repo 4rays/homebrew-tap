561b7f864b490375e85d6b989a8650c54e910782
Reda Lemeden <git@redalemeden.com>
2026-01-16 18:15:52 +0100

    Use standard venv with pip install from PyPI

--- Formula/ask-deepwiki.rb
+++ Formula/ask-deepwiki.rb
@@ -1,6 +1,4 @@
 class AskDeepwiki < Formula
-  include Language::Python::Virtualenv
-
   desc "CLI tool for querying DeepWiki documentation"
   homepage "https://github.com/4rays/ask-deepwiki"
   url "https://files.pythonhosted.org/packages/cc/e6/bfad173b9ee226c440a787cdc7b4fd3c3d6bd82ab09d759125412a468501/ask_deepwiki-0.1.0.tar.gz"
@@ -10,8 +8,10 @@ class AskDeepwiki < Formula
   depends_on "python@3.12"
 
   def install
-    venv = virtualenv_create(libexec, "python3.12", system_site_packages: false)
-    venv.pip_install "ask-deepwiki==#{version}"
+    python3 = "python3.12"
+    system python3, "-m", "venv", libexec
+    system libexec/"bin/pip", "install", "--upgrade", "pip"
+    system libexec/"bin/pip", "install", "ask-deepwiki==#{version}"
     bin.install_symlink libexec/"bin/ask-deepwiki"
   end
 
