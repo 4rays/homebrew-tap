6f2e2ce77e81755bfc734ba6e2633c7856ef0e6a
Reda Lemeden <git@redalemeden.com>
2026-04-14 11:46:16 +0200

    Fix xhammer SHA256 (repo made public)

--- Formula/xhammer.rb
+++ Formula/xhammer.rb
@@ -2,7 +2,7 @@ class Xhammer < Formula
   desc "CLI + daemon for Xcode MCP bridge access"
   homepage "https://github.com/4rays/xhammer"
   url "https://github.com/4rays/xhammer/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "e672970e9d187ad7b6341ba26e53748754111292c917f74d13ac71c9a7d140fa"
   license "MIT"
 
   depends_on xcode: ["15.0", :build]
