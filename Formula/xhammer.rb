bf43f09a876a2c25d81ae309f1467b255173bb9b
Reda Lemeden <git@redalemeden.com>
2026-04-14 11:49:11 +0200

    Use pre-built binaries for xhammer (no Xcode required)

--- Formula/xhammer.rb
+++ Formula/xhammer.rb
@@ -1,17 +1,16 @@
 class Xhammer < Formula
   desc "CLI + daemon for Xcode MCP bridge access"
   homepage "https://github.com/4rays/xhammer"
-  url "https://github.com/4rays/xhammer/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "e672970e9d187ad7b6341ba26e53748754111292c917f74d13ac71c9a7d140fa"
+  url "https://github.com/4rays/xhammer/releases/download/v0.1.0/xhammer-0.1.0-macos.tar.gz"
+  sha256 "ca21c5b6c3a49d192af34d220b91f7c6a7c4064bf708140fda86e68bf3f2ac8a"
+  version "0.1.0"
   license "MIT"
 
-  depends_on xcode: ["15.0", :build]
   depends_on :macos
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/xhammer"
-    bin.install ".build/release/xhammerd"
+    bin.install "xhammer-bin/xhammer"
+    bin.install "xhammer-bin/xhammerd"
   end
 
   test do
