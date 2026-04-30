de2c45e46fce40e71c4738c4cc478701ea3c62f9
Reda Lemeden <git@redalemeden.com>
2026-04-30 16:27:22 +0200

    Update xbridge to 0.1.3

--- Formula/xbridge.rb
+++ Formula/xbridge.rb
@@ -1,16 +1,16 @@
 class Xbridge < Formula
   desc "CLI + daemon for Xcode MCP bridge access"
   homepage "https://github.com/4rays/xbridge"
-  url "https://github.com/4rays/xhammer/releases/download/v0.1.2/xhammer-0.1.2-macos.tar.gz"
-  sha256 "c5a03e0dc34f1e2241f93d34f04787ff29a461eb40c3e8deb1b0da429e54834f"
-  version "0.1.2"
+  url "https://github.com/4rays/xbridge/releases/download/v0.1.3/xbridge-0.1.3-macos.tar.gz"
+  sha256 "33e483ac06f12033200ae22539c202bb040db23535ee3cced1b1ca3cc1eaa7fc"
+  version "0.1.3"
   license "MIT"
 
   depends_on :macos
 
   def install
-    bin.install "xhammer" => "xbridge"
-    bin.install "xhammerd" => "xbridged"
+    bin.install "xbridge"
+    bin.install "xbridged"
   end
 
   test do
